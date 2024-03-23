1ed1f1c62cc07f7748d006b24924f82e823c6f48
derkaiserr <oluadebojnr@gmail.com>
2024-03-23 23:52:30 +0100

    update

--- src/home.jsx
+++ src/home.jsx
@@ -6,12 +6,12 @@ import { Eye } from "lucide-react";
 const PageOne = () => {
   const [eye, setEye] = useState(false);
   return (
-    <div>
+    <div className="mb-24">
       <img src={bg} className="relative cover w-full" alt="" />
 
-      <section className="absolute py-5 px-3  flex flex-col justify-center bg-[#1B5C58] rounded-lg text-white  w-[80%]  -my-24  left-0 right-0  mx-auto ight-4">
+      <section className="absolute py-7 px-5  flex flex-col justify-center bg-[#1B5C58] rounded-3xl text-white  w-[90%]  -my-28  left-0 right-0  mx-auto">
         <div className="flex  justify-between">
-          <p className="flex  items-center text-sm gap-2">
+          <p className="flex font-semibold  items-center text-sm gap-2">
             Total Balance
             <span className="">
               {eye ? (
@@ -50,85 +50,78 @@ const PageOne = () => {
         </div>
 
         <div>
-          
-            <p
-              data-state={eye}
-              className=" data-[state=true]:before:backdrop-blur-md before:bg-[#2f7e79f1  before:w-[95%] before:py-4 before:backdrop-blur-0 text-2xl  before:absolute font-semibold"
-              
-            >
-              &#36;2,500,000.00
-            </p>
+          <p
+            data-state={eye}
+            className=" data-[state=true]:before:backdrop-blur-md before:bg-[#2f7e79f1  before:w-[95%] before:py-4 before:backdrop-blur-0 text-2xl  before:absolute font-semibold"
+          >
+            &#36;2,500,000.00
+          </p>
 
-            <div className="flex justify-between">
-
-            
+          <div className="flex justify-between pt-9">
             <div className="">
-            <div className=" flex">
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-move-up"
-                >
-                  <path d="M8 6L12 2L16 6" />
-                  <path d="M12 2V22" />
-                </svg>
-              </span>
-              Income
+              <div className="items-center text-sm font-semibold text-[#ffffffc6]  justify-start gap-1 flex">
+                <span className="rounded-full p-1  bg-[#ffffff41]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="15"
+                    height="15"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-move-down"
+                  >
+                    <path d="M8 18L12 22L16 18" />
+                    <path d="M12 2V22" />
+                  </svg>
+                </span>
+                Income
+              </div>
+              <p
+                data-state={eye}
+                className=" data-[state=true]:before:backdrop-blur-sm justify-center  flex font-semibold before:bg-[#2f7e793f before:w-[95%] before:py-3 before:backdrop-blur-0  before:absolute"
+              >
+                &#36;1800.00
+              </p>
             </div>
-            <p
-              data-state={eye}
-              className=" data-[state=true]:before:backdrop-blur-sm before:bg-[#2f7e793f before:w-[95%] before:py-3 before:backdrop-blur-0  before:absolute"
-            >
-              &#36;1800.00
-            </p>
-          </div>
 
-          <div>
-            <div className="flex">
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  class="lucide lucide-move-down"
-                >
-                  <path d="M8 18L12 22L16 18" />
-                  <path d="M12 2V22" />
-                </svg>
-              </span>
-              Expenses
+            <div className="font-semibold">
+              <div className="flex gap-1 text-sm  text-[#ffffffc6] items-center">
+                <span className="rounded-full text-white p-1 bg-[#ffffff41]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="14"
+                    height="14"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    class="lucide lucide-move-up"
+                  >
+                    <path d="M8 6L12 2L16 6" />
+                    <path d="M12 2V22" />
+                  </svg>
+                </span>
+                Expenses
+              </div>
+              <p data-state={eye} className=" flex justify-end">
+                &#36;2,500.00
+              </p>
             </div>
-            <p
-              data-state={eye}
-              className=" data-[state=true]:before:backdrop-blur-sm before:bg-[#2f7e793f before:w-[95%] before:py-3 before:backdrop-blur-0  before:absolute"
-            >
-              &#36;2,500.00
-            </p>
-          </div>
           </div>
         </div>
       </section>
-      <header>
-        <p>Transactions History</p>
-        <p>see all</p>
+      <header className="mt-28 flex justify-between px-6 ">
+        <p className="font-semibold">Transactions History</p>
+        <p className="text-sm text-gray-500 font-semibold">see all</p>
       </header>
 
-      <main>
-        <div className="transActs">
+      <main className="px-6">
+        <div className="transActs ">
           <div>
             <p>Upwork</p>
             <p>Today</p>
@@ -157,8 +150,7 @@ const PageOne = () => {
           <p className="amount">+550</p>
         </div>
       </main>
-                  <div className="fixed bg-[#1F615C]  rounded-full p-3 bottom-8 left-0 right-0 mx-auto shadow-lg shadow-slate-300 z-30 text-white w-fit">
-
+      <div className="fixed bg-[#1F615C]  rounded-full p-3 bottom-8 left-0 right-0 mx-auto shadow-lg shadow-slate-300 z-30 text-white w-fit">
         <svg
           className=""
           xmlns="http://www.w3.org/2000/svg"
@@ -175,9 +167,8 @@ const PageOne = () => {
           <path d="M5 12h14" />
           <path d="M12 5v14" />
         </svg>
-          </div>
-      <footer className="fixed bottom-0 w-full  grid px-5 grid-cols-3 items-center py-3 pt-5 border border-t-2">
-
+      </div>
+      <footer className="fixed bottom-0 w-full bg-white grid px-5 grid-cols-3 items-center py-3 pt-5 border border-t-2">
         <div className="flex justify-between">
           <svg
             className="group-[.footIcons]:bg-blue-400"
