043aaca40ef0e88139e25fc9c977b3531d99da38
derkaiserr <oluadebojnr@gmail.com>
2024-06-17 01:29:04 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": {
-        target: "https://v6.exchangerate-api.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
+      "/api": "https://v6.exchangerate-api.com",
     },
   },
   plugins: [react()],
