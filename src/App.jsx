f09333b3b0b3f458b2e4970385dd2df08d8df439
derkaiserr <oluadebojnr@gmail.com>
2024-06-17 03:17:01 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -93,7 +93,9 @@ function App() {
   console.log(API_KEY);
 
   const currencyMemo = useMemo(() => {
-    return fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`)
+    return fetch(
+      `https://v6.exchangerate-api.com/v6/5395552a5b807960dbf0f7df/latest/USD`
+    )
       .then((response) => {
         if (!response.ok) {
           throw new Error("Failed to fetch exchange rate");
