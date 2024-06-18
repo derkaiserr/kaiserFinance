f496c56f135b69e2a451b3bf31783fb43054eee5
derkaiserr <oluadebojnr@gmail.com>
2024-06-18 16:00:28 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ function App() {
 
   const currencyMemo = useMemo(() => {
     return fetch(
-      `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`
+      `https://v6.exchangerate-api.com/v6/5395552a5b807960dbf0f7df/latest/USD`
     )
       .then((response) => {
         if (!response.ok) {
